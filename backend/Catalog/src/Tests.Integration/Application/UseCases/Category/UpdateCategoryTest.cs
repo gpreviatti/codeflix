952ee8d2968dbee098e5386efdd91b512216eb23
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-09-12 21:04:35 -0300

    test: fix erro tracking ef core

--- backend/Catalog/src/Tests.Integration/Application/UseCases/Category/UpdateCategoryTest.cs
+++ backend/Catalog/src/Tests.Integration/Application/UseCases/Category/UpdateCategoryTest.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Dtos.Category;
 using Application.Interfaces.UseCases;
 using Application.UseCases.Category;
-using Tests.Common.Generators;
 using Tests.Common.Generators.Entities;
 
 namespace Tests.Integration.Application.UseCases.Category;
@@ -15,28 +14,27 @@ public class UpdateCategoryTest : CategoryTestFixture
         _updateCategory = new UpdateCategory(_categoryRepository, _unitOfWork);
     }
 
-    //[Fact]
-    //[Trait("Integration", "Update - Use Cases")]
-    //public async Task Update()
-    //{
-    //    var category = CategoryGenerator.GetCategory();
-    //    await dbContext.AddAsync(category);
-    //    await dbContext.SaveChangesAsync();
+    [Fact]
+    [Trait("Integration", "Update - Use Cases")]
+    public async Task Update()
+    {
+        var category = CategoryGenerator.GetCategory();
+        var trackingInfo = await dbContext.AddAsync(category, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+        
+        // Fix tracking problem in ef core
+        trackingInfo.State = Microsoft.EntityFrameworkCore.EntityState.Detached;
 
-    //    var newName = Faker.Commerce.ProductName();
-    //    var input = new UpdateCategoryInput(category.Id, newName);
+        var newName = Faker.Commerce.ProductName();
+        var input = new UpdateCategoryInput(category.Id, newName);
 
-    //    //dbContext = CreateDbContext(Guid.NewGuid());
-    //    //_categoryRepository = new CategoryRepository(dbContext);
-    //    //_updateCategory = new UpdateCategory(_categoryRepository, _unitOfWork);
+        var output = await _updateCategory.Handle(input, CancellationToken.None);
 
-    //    var output = await _updateCategory.Handle(input, CancellationToken.None);
-
-    //    output.Should().NotBeNull();
-    //    output.GetType().Should().Be<CategoryOutput>();
-    //    output.Id.Should().Be(category.Id);
-    //    output.Name.Should().Be(newName);
-    //    output.Description.Should().Be(category.Description);
-    //    output.CreatedAt.Should().NotBe(default);
-    //}
+        output.GetType().Should().Be<CategoryOutput>().And.NotBeNull();
+        output.Id.Should().Be(category.Id);
+        output.Name.Should().Be(newName);
+        output.Description.Should().Be(category.Description);
+        output.IsActive.Should().Be(category.IsActive);
+        output.CreatedAt.Should().NotBe(default);
+    }
 }
