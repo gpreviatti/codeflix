05e9478a278d607193192a0d8d4f3af1f6e58400
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-09-20 19:33:18 -0300

    add integration UpdateOnlyName

--- backend/Catalog/src/Tests.Integration/Api/Category/UpdateCategoryApiTest.cs
+++ backend/Catalog/src/Tests.Integration/Api/Category/UpdateCategoryApiTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using Tests.Common.Generators.Dtos;
+using Tests.Common.Generators.Entities;
 
 namespace Tests.Integration.Api.Category;
 public class UpdateCategoryApiTest : CategoryApiTestFixture
@@ -29,6 +30,35 @@ public class UpdateCategoryApiTest : CategoryApiTestFixture
         output!.Name.Should().Be(inputUpdate.Name);
         output!.Description.Should().Be(inputUpdate.Description);
         output!.IsActive.Should().BeTrue();
+        output!.CreatedAt.Should().NotBe(default);
+    }
+
+    [Fact(DisplayName = nameof(UpdateOnlyName))]
+    [Trait("Integration/Api", "Category - Update")]
+    public async Task UpdateOnlyName()
+    {
+        var inputCreate = CreateCategoryInputGenerator.GetCategoryInput();
+
+        var (responseCreate, outputCreate) = await apiClient
+            .Post<CategoryOutput>(RESOURCE_URL, inputCreate);
+
+        var inputUpdate = new UpdateCategoryInput(
+            outputCreate!.Id, 
+            CategoryGenerator.GetProductName()
+        );
+
+        var (response, output) = await apiClient
+            .Put<CategoryOutput>(RESOURCE_URL, inputUpdate);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        output.Should().NotBeNull();
+        output!.GetType().Should().Be<CategoryOutput>().And.NotBeNull();
+        output!.Id.Should().Be(outputCreate.Id);
+        output!.Name.Should().Be(inputUpdate.Name);
+        output!.Description.Should().Be(outputCreate.Description);
+        output!.IsActive.Should().Be(outputCreate.IsActive);
         output!.CreatedAt.Should().NotBe(default);
     }
 
