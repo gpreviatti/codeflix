a21a1c45e8095831e5b1a3c6cab6297c7cc91818
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2022-09-24 13:38:27 -0300

    add vscode support

--- backend/Catalog/src/Tests.Integration/Api/Common/CustomWebApplicationFactory.cs
+++ backend/Catalog/src/Tests.Integration/Api/Common/CustomWebApplicationFactory.cs
@@ -1,7 +1,7 @@
 ﻿using Infra.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace Tests.Integration.Api.Common;
 
@@ -9,19 +9,14 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Test");
-        builder.ConfigureServices(services =>
-        {
-            var serviceProvider = services.BuildServiceProvider();
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.Test.json")
+            .Build();
 
-            using var scope = serviceProvider.CreateScope();
-
-            var context = scope.ServiceProvider.GetService<CatalogDbContext>();
-            ArgumentNullException.ThrowIfNull(context);
-
-            //context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-        });
+        builder
+            .UseEnvironment("Test")
+            .UseConfiguration(configuration);
 
         base.ConfigureWebHost(builder);
     }
